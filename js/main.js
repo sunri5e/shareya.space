680fd21c1d3eb9325cc15dfa90ef0bca4d0cba66
sunri5e <sunrise944@gmail.com>
2016-03-10 00:01:42 +0200

    Avalible places - full markup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-$(document).ready(function(){
-  $('.cw-articles-nav--cat').on('click', function(e){
-    e.preventDefault();
-    $(this).parent().find('ul').slideToggle();
-    $(this).toggleClass('open');
+function custAddClass(el, mod){
+	$(el).addClass(mod);
+}
+
+function custRemoveClass(el, mod){
+	$(el).removeClass(mod);
+}
+
+function showAllDays() {
+	$('.sy-av-section').show();
+}
+
+$(document).ready(function() {
+  $("#datepicker").datepicker({
+    prevText: '<i class="zmdi zmdi-chevron-left"></i>',
+    nextText: '<i class="zmdi zmdi-chevron-right"></i>',
+    beforeShowDay: $.datepicker.noWeekends,
+    beforeShow: function(){
+    	custAddClass('.sy-overlay', 'is-opened');
+    },
+    altField: "#alternate",
+    altFormat: "d MM ("+ 5 +" available)",
+    dateFormat: "yy-mm-dd",
+    onClose: function(){
+    	custRemoveClass('.sy-overlay', 'is-opened');
+    	custAddClass('.sy-filter--query', 'is-visibale');
+    	showAllDays()
+    	$('.sy-av-section:not([data-day="'+$("#datepicker").val()+'"])').hide();
+    }
+  });
+
+  $('.sy-filter--clear-query').on('click', function(){
+    custRemoveClass('.sy-filter--query', 'is-visibale');
+  	$("#datepicker").datepicker( "setDate", "" );
+  	showAllDays();
   });
 });
