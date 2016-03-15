f13fe857456fd195cedc4c6bffd76af2d4a57e82
sunri5e <sunrise944@gmail.com>
2016-03-15 21:01:01 +0200

    place page

--- js/main.js
+++ js/main.js
@@ -11,6 +11,21 @@ function showAllDays() {
 }
 
 $(document).ready(function() {
+	if ($('.sy-header').length) {
+		$('body').addClass('with-header');
+	}
+	if ($('.sy-footer').length) {
+		$('body').addClass('with-footer');
+	}
+
+
+	$('.sy-overlay, .sy-overlay-close').on('click', function(e){
+		e.stopPropagation();
+		if (e.target == this) {
+			custRemoveClass('.sy-overlay', 'is-opened');
+    }
+	});
+
   $("#datepicker").datepicker({
     prevText: '<i class="zmdi zmdi-chevron-left"></i>',
     nextText: '<i class="zmdi zmdi-chevron-right"></i>',
@@ -28,15 +43,28 @@ $(document).ready(function() {
     	$('.sy-av-section:not([data-day="'+$("#datepicker").val()+'"])').hide();
     }
   });
-
   $('.sy-filter--clear-query').on('click', function(){
     custRemoveClass('.sy-filter--query', 'is-visibale');
   	$("#datepicker").datepicker( "setDate", "" );
   	showAllDays();
   });
-
   $('#sharing-calendar').multiDatesPicker({
   	prevText: '<i class="zmdi zmdi-chevron-left"></i>',
     nextText: '<i class="zmdi zmdi-chevron-right"></i>'
   });
+
+  $('.sy-multiselect-item label').on('click', function(e){
+  	if ($(this).parent().hasClass('is-already-booked')) {
+  		e.preventDefault();
+  		custAddClass('.sy-overlay', 'is-opened');
+  	}
+  });
 });
+
+
+
+
+
+
+
+
